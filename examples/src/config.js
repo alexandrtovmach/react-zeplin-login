fefdf9261f0c8c867237611984fb81269363d2ea
alexandrtovmach <alexandrtovmach@gmail.com>
2019-12-26 23:12:38 +0200

    feat: Added valid clientId and clientSecret

--- examples/src/config.js
+++ examples/src/config.js
@@ -1,6 +1,6 @@
 export default {
-  clientId: "FLGxXFmPm4cvrmqS3aU8dp",
-  clientSecret: "dN2tzjsgJ530NPQOButqXoGPhB4Nhv",
+  clientId: "5df1295ce8023519f7598b7f",
+  clientSecret: "b7f216c3-7a14-479f-9b7f-eafd0de72bfa",
   customClassName: "my-custom-class",
   themeOptions: [
     {
